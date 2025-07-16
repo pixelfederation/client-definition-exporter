281115f0c536d9ac623ff204671e329e417d9c44
Tomas Hulata <tombo@sysart.tech>
2025-07-16 14:53:41 +0200

    add verbosity option and fixes

--- config.go
+++ config.go
@@ -2,10 +2,11 @@ package main
 
 // Ops represents the commandline/environment options for the program
 type ConfigOpts struct {
-	BaseDir         string `long:"basedir" short:"d" env:"BASEDIR" default:"/srv/www/" description:"Definition location"`
-	MetricsBindAddr string `long:"metrics-bind-address" short:"b" env:"METRICS_BIND_ADDRESS" default:"9115" description:"Address for binding metrics listener"`
-	MetricsPath     string `long:"metrics-path" env:"METRICS_PATH" default:"/metrics" description:"Metrics path"`
-	FilenameFormat  string `long:"filename-format" env:"FILENAME_FORMAT" default:"client-data" description:"Definition files name format"`
-	FileExtension   string `long:"file-extension" env:"FILE_EXTENSION" default:"sqlite" description:"Definition files extension"`
-	Refresh         int    `long:"refresh" short:"r" env:"REFRESH" default:"60" description:"Definition refresh rate"`
+    BaseDir         string `long:"basedir" short:"d" env:"BASEDIR" default:"/srv/www/" description:"Definition location"`
+    MetricsBindAddr string `long:"metrics-bind-address" short:"b" env:"METRICS_BIND_ADDRESS" default:"9115" description:"Address for binding metrics listener"`
+    MetricsPath     string `long:"metrics-path" short:"m" env:"METRICS_PATH" default:"/metrics" description:"Metrics path"`
+    FilenameFormat  string `long:"filename-format" short:"f" env:"FILENAME_FORMAT" default:"client-data" description:"Definition files name format"`
+    FileExtension   string `long:"file-extension" short:"e" env:"FILE_EXTENSION" default:"sqlite" description:"Definition files extension"`
+    Refresh         int    `long:"refresh" short:"r" env:"REFRESH" default:"60" description:"Definition refresh rate"`
+    LogLevel        int    `long:"loglevel" short:"v" env:"LOG_LEVEL" description:"log verbosity level for klog" default:"2"`
 }
--- main.go
+++ main.go
@@ -1,87 +1,99 @@
 package main
 
 import (
-	"fmt"
-	"io/fs"
-	"net/http"
-	"path/filepath"
-	"regexp"
-	"strings"
-	"time"
+    "flag"
+    "fmt"
+    "io/fs"
+    "net/http"
+    "path/filepath"
+    "regexp"
+    "strings"
+    "time"
 
-	"github.com/jessevdk/go-flags"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"k8s.io/klog/v2"
+    "github.com/jessevdk/go-flags"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+    "k8s.io/klog/v2"
 )
 
 var FilenameFormat, FileExtension = "client - data", "sqlite"
 
 func visit(path string, di fs.DirEntry, err error) error {
-	// r, _ := regexp.Compile("client-data.*.sqlite")
-	r, _ := regexp.Compile(FilenameFormat + ".*." + FileExtension)
+    // r, _ := regexp.Compile("client-data.*.sqlite")
+    r, _ := regexp.Compile(FilenameFormat + ".*." + FileExtension)
 
-	if r.MatchString(path) {
-		klog.Info("Visited: %s\n", filepath.Base(path))
+    if r.MatchString(path) {
+        klog.V(4).Info("Visited: %s\n", filepath.Base(path))
 
-		decomposed_f := strings.Split(path, "/")
-		dynamic := decomposed_f[len(decomposed_f)-2]
+        decomposed_f := strings.Split(path, "/")
+        dynamic := decomposed_f[len(decomposed_f)-2]
 
-		if dynamic == "client-resources" {
-			dynamic = "none"
-		}
+        if dynamic == "client-resources" {
+            dynamic = "none"
+        }
 
-		if decomposed_f[4] == "" {
-			klog.Fatalf("Error decompose path: %v", err)
-		}
+        if decomposed_f[4] == "" {
+            klog.Fatalf("Error decompose path: %v", err)
+        }
 
-		clienDefinitionMetrics(prometheus.Labels{
-			"origin":  decomposed_f[4],
-			"env":     decomposed_f[3],
-			"dynamic": dynamic,
-			// "version": strings.ReplaceAll(filepath.Base(path), ".sqlite", "")})
-			"version": strings.ReplaceAll(filepath.Base(path), "."+FileExtension, "")})
-	}
-	return nil
+        clienDefinitionMetrics(prometheus.Labels{
+            "origin":  decomposed_f[4],
+            "env":     decomposed_f[3],
+            "dynamic": dynamic,
+            // "version": strings.ReplaceAll(filepath.Base(path), ".sqlite", "")})
+            "version": strings.ReplaceAll(filepath.Base(path), "."+FileExtension, "")})
+    }
+    return nil
 }
 
 func clienDefinitionMetrics(labels map[string]string) {
-	if err := prometheus.Register(prometheus.NewGaugeFunc(
-		prometheus.GaugeOpts{
-			Namespace:   "client",
-			Name:        "definition_data_version",
-			Help:        "Client definition data version.",
-			ConstLabels: labels,
-		},
-		func() float64 { return float64(1) },
-	)); err == nil {
-		klog.Info("GaugeFunc 'definition_data_version' registered with labels: %s \n", labels)
-	}
+    if err := prometheus.Register(prometheus.NewGaugeFunc(
+        prometheus.GaugeOpts{
+            Namespace:   "client",
+            Name:        "definition_data_version",
+            Help:        "Client definition data version.",
+            ConstLabels: labels,
+        },
+        func() float64 { return float64(1) },
+    )); err == nil {
+        klog.V(4).Info("GaugeFunc 'definition_data_version' registered with labels: %s \n", labels)
+    }
 }
 
 func recordMetrics(conf *ConfigOpts) {
-	ticker := time.NewTicker(time.Duration(conf.Refresh) * time.Second)
-	for {
-		err := filepath.WalkDir(conf.BaseDir, visit)
-		fmt.Printf("filepath.WalkDir() returned %v\n", err)
-		<-ticker.C
-	}
+    ticker := time.NewTicker(time.Duration(conf.Refresh) * time.Second)
+    for {
+        err := filepath.WalkDir(conf.BaseDir, visit)
+        if err != nil {
+            fmt.Printf("filepath.WalkDir() returned %v\n", err)
+        }
+        <-ticker.C
+    }
 }
 
 func main() {
 
-	klog.InitFlags(nil)
-	conf := &ConfigOpts{}
-	parser := flags.NewParser(conf, flags.Default)
-	if _, err := parser.Parse(); err != nil {
-		klog.Fatalf("Error parsing flags: %v", err)
-	}
+    klog.InitFlags(nil)
 
-	FileExtension = conf.FileExtension
-	FilenameFormat = conf.FilenameFormat
+    var conf ConfigOpts
+    _, err := flags.Parse(&conf)
 
-	go recordMetrics(conf)
+    if err != nil {
+      panic("no arguments")
+    }
 
-	http.Handle(conf.MetricsPath, promhttp.Handler())
-	http.ListenAndServe(":"+conf.MetricsBindAddr, nil)
+    flag.Set("v", fmt.Sprintf("%d", conf.LogLevel))
+
+    defer klog.Flush()
+
+    FileExtension = conf.FileExtension
+    FilenameFormat = conf.FilenameFormat
+
+    go recordMetrics(&conf)
+
+    http.Handle(conf.MetricsPath, promhttp.Handler())
+    err = http.ListenAndServe(":"+conf.MetricsBindAddr, nil)
+    if err != nil {
+        klog.Fatalf("Failed to start server: %v", err)
+    }
 }
